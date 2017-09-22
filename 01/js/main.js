c48c7c449abc0f34b27191d18a87d5d0749abaa8
Your Name <you@example.com>
2017-09-22 08:28:32 +0200

    JS

---
+++ 01/js/main.js
@@ -0,0 +1,5 @@
+console.log('Funguje to');
+console.log(document.getElementById('zahlavi'));
+document.getElementById('zahlavi').style.backgroundColor = 'yellow';
+document.getElementById('nadpis').innerText = prompt('Zadej text');
+document.getElementsByTagName('img')[0].width / = 2;
